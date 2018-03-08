5f14552602e2b4963dd482edd60ed4dbf5f6af8d
Eduardo Lima <edulima2412@gmail.com>
2018-03-08 16:09:08 -0300

    programando c#

--- CaixasDialogo/CaixasDialogo/Form1.Designer.cs
+++ CaixasDialogo/CaixasDialogo/Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_msgbox = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_folder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_msgbox
+            // 
+            this.btn_msgbox.Location = new System.Drawing.Point(226, 12);
+            this.btn_msgbox.Name = "btn_msgbox";
+            this.btn_msgbox.Size = new System.Drawing.Size(113, 29);
+            this.btn_msgbox.TabIndex = 0;
+            this.btn_msgbox.Text = "MessageBox";
+            this.btn_msgbox.UseVisualStyleBackColor = true;
+            this.btn_msgbox.Click += new System.EventHandler(this.btn_msgbox_Click);
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(226, 47);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(113, 29);
+            this.btn_open.TabIndex = 1;
+            this.btn_open.Text = "OpenFileDialog";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 204);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(226, 82);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(113, 29);
+            this.btn_save.TabIndex = 3;
+            this.btn_save.Text = "SaveFileDialog";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_folder
+            // 
+            this.btn_folder.Location = new System.Drawing.Point(226, 117);
+            this.btn_folder.Name = "btn_folder";
+            this.btn_folder.Size = new System.Drawing.Size(113, 29);
+            this.btn_folder.TabIndex = 4;
+            this.btn_folder.Text = "FolderBrowserDialog";
+            this.btn_folder.UseVisualStyleBackColor = true;
+            this.btn_folder.Click += new System.EventHandler(this.btn_folder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 228);
+            this.Controls.Add(this.btn_folder);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_msgbox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_msgbox;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_folder;
     }
 }
 
--- CaixasDialogo/CaixasDialogo/Form1.cs
+++ CaixasDialogo/CaixasDialogo/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Windows.Forms;
 
 namespace CaixasDialogo
@@ -14,6 +15,62 @@ namespace CaixasDialogo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_msgbox_Click(object sender, EventArgs e)
+        {
+            //Titulo, botão, ícone
+            if(MessageBox.Show("Deseja encerrar o programa", "Programa Teste", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return;
+            }
+            else
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog caixa = new OpenFileDialog();
+            caixa.Title = "Abrindo ficheiro";
+            caixa.InitialDirectory = @"C:\";
+            caixa.Filter = "Jpeg Images (*.jpg)|*.jpg|" +
+                           "BMP Images (*.bmp)|*.bmp";
+
+            if (caixa.ShowDialog() == DialogResult.Cancel) return;
+
+            pictureBox1.BackgroundImage = Image.FromFile(caixa.FileName);
+
+            caixa.Dispose();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog caixa = new SaveFileDialog();
+            caixa.Title = "Salvando ficheiro";
+            caixa.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            caixa.Filter = "TXT Files (*.txt)|*.txt|" +
+                           "JPG Images (*.jpg)|*.jpg|" +
+                           "BMP Images (*.bmp)|*.bmp";
+            if (caixa.ShowDialog() == DialogResult.Cancel) return;
+
+            //Salvar ficheiro
+            string ficheiro = caixa.FileName;
+
+            StreamWriter file = new StreamWriter(ficheiro, false, Encoding.Default);
+            file.WriteLine("Testando");
+            file.Dispose();
+        }
+
+        private void btn_folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog caixa = new FolderBrowserDialog();
+            caixa.SelectedPath = @"C:\";
+            if (caixa.ShowDialog() == DialogResult.Cancel) return;
+
+            MessageBox.Show("A pasta escolhida foi " + caixa.SelectedPath);
         }
     }
 }
